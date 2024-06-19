bc933e6c55d545ec923da0edbffd5cf40bdfde27
Harish Tadikamalla <harish.tadikamalla@tredence.com>
2024-06-19 04:58:49 +0000

    commit

---
+++ macros/get_payment_methods.sql
@@ -0,0 +1,20 @@
+{% macro get_payment_methods() %}
+
+    {% set payment_methods_query %}
+select distinct
+payment_method
+from {{ ref ('raw_payments') }}
+order by 1
+{% endset %}
+
+    {% set results = run_query(payment_methods_query) %}
+
+    {% if execute %}
+
+        {% set results_list = results.columns[0].values %}
+    {% else %}
+        {% set results_list = [] %}
+    {% endif %}
+
+    {{ return(results_list) }}
+{% endmacro %}
